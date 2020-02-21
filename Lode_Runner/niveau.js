badd12d4185ce0fa8ae5e4d106c5cd73678ca2bb
Caroline <Caroline@MacBook-Air-de-Lise-2.local>
2020-02-21 09:20:17 -0500

    Dessiner échelle, lingots, béton et barre

--- Lode_Runner/niveau.js
+++ Lode_Runner/niveau.js
@@ -34,6 +34,62 @@ function dessinerBrique(){
     objC2D.restore();
 }
 
+function dessinerEchellePartielle(){
+    objC2D.save();
+    objC2D.fillStyle= '#336600';
+
+    objC2D.beginPath();
+    objC2D.fillRect(0,0,5,30);
+    objC2D.fillRect(25,0,5,30);
+    objC2D.fillRect(0,5,30,3);
+    objC2D.fillRect(0,15,30,3);
+    objC2D.fillRect(0,25,30,3);
+
+    objC2D.restore();
+}
+
+function dessinerLingot(){
+    objC2D.save();
+
+    let objDegrade=objC2D.createLinearGradient(5,16,25,30);
+
+    objDegrade.addColorStop(0,'#F7BC35');
+    objDegrade.addColorStop(0.5,'#E1DEDD');
+    objDegrade.addColorStop(1,'#F7BC35');
+
+    objC2D.fillStyle=objDegrade;
+    objC2D.beginPath();
+    objC2D.fillRect(5,16,20,14);
+
+    objC2D.restore();
+}
+
+function dessinerBeton(){
+    objC2D.save();
+
+    objC2D.fillStyle='#9e9c9c';
+    objC2D.beginPath();
+    objC2D.fillRect(0,0,30,30);
+
+    objC2D.fillStyle='#CBC9CD';
+    objC2D.beginPath();
+    objC2D.fillRect(5,5,20,20);
+
+    objC2D.restore();
+
+}
+
+function dessinerBarre(){
+    objC2D.save();
+
+    objC2D.fillStyle='#a09794';
+
+    objC2D.beginPath();
+    objC2D.fillRect(0,5,30,3);
+
+    objC2D.restore();
+}
+
 function dessinerPlateforme(){
     objC2D.save();
     for(let i = 0;i<5;i++){
@@ -45,10 +101,28 @@ function dessinerPlateforme(){
     objC2D.restore();
 }
 
+function dessinerEchelleComplete(){
+    for(let y = 0;y<5;y++){
+        objC2D.save();
+        objC2D.translate(0,30*y);
+        dessinerEchellePartielle();
+        objC2D.restore();
+    }
+}
+
 function dessinerNiveau(){
     objC2D.save();
     objC2D.translate(50,50);
     dessinerFond();
     dessinerPlateforme();
+    dessinerEchelleComplete();
+    dessinerLingot();
+
+    objC2D.translate(100,0);
+    dessinerBarre();
+
+
+    objC2D.translate(100,0);
+    dessinerBeton();
     objC2D.restore();
 }

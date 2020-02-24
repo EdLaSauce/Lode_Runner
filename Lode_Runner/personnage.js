34dc46f6d53852b1c8a5c30ef0e974ebd480cc18
Caroline <carolineboudreau@live.ca>
2020-02-24 12:26:29 -0500

    Modification grandeur personnages

--- Lode_Runner/personnage.js
+++ Lode_Runner/personnage.js
@@ -16,52 +16,50 @@ function dessinerLodeRunner() {
     objC2D.translate(objLodeRunner.posX, objLodeRunner.posY);
 
     objC2D.beginPath();
-    objC2D.arc(6, 6, 5, 0, 2 * Math.PI);
+    objC2D.arc(15, 6, 5, 0, 2 * Math.PI);
     objC2D.fill();
 
     objC2D.fillStyle = objLodeRunner.strCouleur;
 
     //milieu du corps
-    objC2D.fillRect(4, 11, 4.5, 10);
+    objC2D.fillRect(13, 11, 4.5, 10);
 
     //bras gauche
     objC2D.save();
     objC2D.rotate(2 * Math.PI / 16);
-    objC2D.fillRect(4.5, 11, 5.5, 3.3);
+    objC2D.fillRect(12 , 6, 5.5, 3.3);
     objC2D.restore();
     objC2D.save();
     objC2D.rotate(-1 * Math.PI / 8);
-    objC2D.fillRect(-10, 11, 5, 3.3);
+    objC2D.fillRect(-1, 13, 5, 3.3);
 
     objC2D.restore();
 
     //bras droit
     objC2D.save();
     objC2D.rotate(-2 * Math.PI / 16);
-    objC2D.fillRect(1, 17, 5.5, 3.3);
+    objC2D.fillRect(10, 18, 5.5, 3.3);
     objC2D.restore();
     objC2D.save();
     objC2D.rotate(-2 * Math.PI / 8);
-    objC2D.fillRect(-2, 18, 5, 3.3);
+    objC2D.fillRect(5, 22, 5, 3.3);
     objC2D.restore();
 
     objC2D.fillStyle = '#a09794';
     //jambe gauche
     objC2D.save();
     objC2D.rotate(3.5 * Math.PI / 16);
-    objC2D.fillRect(16, 11, 3.5, 7);
+    objC2D.fillRect(22, 7, 3.5, 7);
     objC2D.rotate(7 * Math.PI / 16);
-    objC2D.fillRect(20, -15, 3.5, 7);
+    objC2D.fillRect(16, -22, 3.5, 7);
     objC2D.restore();
 
     //jambe droite
     objC2D.save();
     objC2D.rotate(-7 * Math.PI / 16);
-    objC2D.fillRect(-22, 10, 3.5, 7);
+    objC2D.fillRect(-20, 18, 3.5, 7);
     objC2D.rotate(-7 * Math.PI / 16);
-    objC2D.fillRect(-22, -23, 3.5, 8);
-    objC2D.restore();
-
+    objC2D.fillRect(-30, -20, 3.5, 8);
     objC2D.restore();
 
     objC2D.restore();
@@ -75,52 +73,50 @@ function dessinerGarde(objGarde) {
     objC2D.translate(objGarde.posX, objGarde.posY);
 
     objC2D.beginPath();
-    objC2D.arc(6, 6, 5, 0, 2 * Math.PI);
+    objC2D.arc(15, 6, 5, 0, 2 * Math.PI);
     objC2D.fill();
 
     objC2D.fillStyle = objGarde.strCouleur;
 
     //milieu du corps
-    objC2D.fillRect(4, 11, 4.5, 10);
+    objC2D.fillRect(13, 11, 4.5, 10);
 
-    //bras gauche
-    objC2D.save();
-    objC2D.rotate(2 * Math.PI / 16);
-    objC2D.fillRect(5, 11, 5.5, 3.3);
-    objC2D.restore();
-    objC2D.save();
-    objC2D.rotate(-1 * Math.PI / 8);
-    objC2D.fillRect(-10, 11, 5, 3.3);
-
-    objC2D.restore();
-
-    //bras droit
-    objC2D.save();
-    objC2D.rotate(-2 * Math.PI / 16);
-    objC2D.fillRect(1, 17, 5.5, 3.3);
-    objC2D.restore();
-    objC2D.save();
-    objC2D.rotate(-2 * Math.PI / 8);
-    objC2D.fillRect(-2, 18, 5, 3.3);
-    objC2D.restore();
+     //bras gauche
+     objC2D.save();
+     objC2D.rotate(2 * Math.PI / 16);
+     objC2D.fillRect(12 , 6, 5.5, 3.3);
+     objC2D.restore();
+     objC2D.save();
+     objC2D.rotate(-1 * Math.PI / 8);
+     objC2D.fillRect(-1, 13, 5, 3.3);
+ 
+     objC2D.restore();
+ 
+     //bras droit
+     objC2D.save();
+     objC2D.rotate(-2 * Math.PI / 16);
+     objC2D.fillRect(10, 18, 5.5, 3.3);
+     objC2D.restore();
+     objC2D.save();
+     objC2D.rotate(-2 * Math.PI / 8);
+     objC2D.fillRect(5, 22, 5, 3.3);
+     objC2D.restore();
 
     objC2D.fillStyle = '#a09794';
     //jambe gauche
     objC2D.save();
     objC2D.rotate(3.5 * Math.PI / 16);
-    objC2D.fillRect(16, 12, 3.5, 7);
+    objC2D.fillRect(22, 7, 3.5, 7);
     objC2D.rotate(7 * Math.PI / 16);
-    objC2D.fillRect(20, -15, 3.5, 7);
+    objC2D.fillRect(16, -22, 3.5, 7);
     objC2D.restore();
 
     //jambe droite
     objC2D.save();
     objC2D.rotate(-7 * Math.PI / 16);
-    objC2D.fillRect(-22, 10, 3.5, 7);
+    objC2D.fillRect(-20, 18, 3.5, 7);
     objC2D.rotate(-7 * Math.PI / 16);
-    objC2D.fillRect(-22, -23, 3.5, 8);
-    objC2D.restore();
-
+    objC2D.fillRect(-30, -20, 3.5, 8);
     objC2D.restore();
 
     objC2D.restore();

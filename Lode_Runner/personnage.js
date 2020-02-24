5f263d9179af3fb02e69840d52b2539ad5d9751b
Caroline <carolineboudreau@live.ca>
2020-02-24 09:53:32 -0500

    ajout dessin garde et petite modification dessin Lode Runner

--- Lode_Runner/personnage.js
+++ Lode_Runner/personnage.js
@@ -7,64 +7,114 @@ function dessinerLodeRunner() {
     objC2D.translate(objLodeRunner.posX, objLodeRunner.posY);
 
     objC2D.beginPath();
-    objC2D.arc(6, 8, 4, 0, 2 * Math.PI);
+    objC2D.arc(6, 6, 5, 0, 2 * Math.PI);
     objC2D.fill();
 
     objC2D.fillStyle = objLodeRunner.strCouleur;
 
-    objC2D.fillRect(4.5, 12, 3.5, 8);
+    //milieu du corps
+    objC2D.fillRect(4, 11, 4.5, 10);
 
     //bras gauche
     objC2D.save();
     objC2D.rotate(2 * Math.PI / 16);
-    objC2D.fillRect(6, 12, 5, 2.5);
+    objC2D.fillRect(4.5, 11, 5.5, 3.3);
     objC2D.restore();
     objC2D.save();
     objC2D.rotate(-1 * Math.PI / 8);
-    objC2D.fillRect(-8, 13, 5, 2.5);
+    objC2D.fillRect(-10, 11, 5, 3.3);
 
     objC2D.restore();
 
-    objC2D.fillStyle = objLodeRunner.strCouleur;
     //bras droit
     objC2D.save();
     objC2D.rotate(-2 * Math.PI / 16);
-    objC2D.fillRect(0, 17, 5, 2.5);
+    objC2D.fillRect(1, 17, 5.5, 3.3);
     objC2D.restore();
     objC2D.save();
     objC2D.rotate(-2 * Math.PI / 8);
-    objC2D.fillRect(-3, 17, 5, 2.5);
+    objC2D.fillRect(-2, 18, 5, 3.3);
     objC2D.restore();
 
     objC2D.fillStyle = '#a09794';
     //jambe gauche
     objC2D.save();
     objC2D.rotate(3.5 * Math.PI / 16);
-    objC2D.fillRect(16, 13, 3, 6);
+    objC2D.fillRect(16, 11, 3.5, 7);
     objC2D.rotate(7 * Math.PI / 16);
-    objC2D.fillRect(20, -14, 3, 6);
+    objC2D.fillRect(20, -15, 3.5, 7);
     objC2D.restore();
 
     //jambe droite
     objC2D.save();
     objC2D.rotate(-7 * Math.PI / 16);
-    objC2D.fillRect(-21, 9, 3, 6);
+    objC2D.fillRect(-22, 10, 3.5, 7);
     objC2D.rotate(-7 * Math.PI / 16);
-    objC2D.fillRect(-21, -21, 3, 6);
+    objC2D.fillRect(-22, -23, 3.5, 8);
+    objC2D.restore();
+
     objC2D.restore();
 
     objC2D.restore();
 }
 
-function dessinerGardes(){
+function dessinerGardes() {
     objC2D.save();
 
     objC2D.fillStyle = '#a09794';
-    objC2D.translate(200, 220);
+
+    //À changer lorsque nous allons avoir la position aléatoire des gardes
+    objC2D.translate(objLodeRunner.posX - 60, objLodeRunner.posY);
 
     objC2D.beginPath();
-    objC2D.arc(6, 8, 4, 0, 2 * Math.PI);
+    objC2D.arc(6, 6, 5, 0, 2 * Math.PI);
     objC2D.fill();
-    
+
+    // À changer lorsque initObjTabGardes sera appelé pour l'objet tabObjGardes[0].objGarde.strCouleur
+    objC2D.fillStyle = 'red';
+
+    //milieu du corps
+    objC2D.fillRect(4, 11, 4.5, 10);
+
+    //bras gauche
+    objC2D.save();
+    objC2D.rotate(2 * Math.PI / 16);
+    objC2D.fillRect(5, 11, 5.5, 3.3);
+    objC2D.restore();
+    objC2D.save();
+    objC2D.rotate(-1 * Math.PI / 8);
+    objC2D.fillRect(-10, 11, 5, 3.3);
+
+    objC2D.restore();
+
+    //bras droit
+    objC2D.save();
+    objC2D.rotate(-2 * Math.PI / 16);
+    objC2D.fillRect(1, 17, 5.5, 3.3);
+    objC2D.restore();
+    objC2D.save();
+    objC2D.rotate(-2 * Math.PI / 8);
+    objC2D.fillRect(-2, 18, 5, 3.3);
+    objC2D.restore();
+
+    objC2D.fillStyle = '#a09794';
+    //jambe gauche
+    objC2D.save();
+    objC2D.rotate(3.5 * Math.PI / 16);
+    objC2D.fillRect(16, 12, 3.5, 7);
+    objC2D.rotate(7 * Math.PI / 16);
+    objC2D.fillRect(20, -15, 3.5, 7);
+    objC2D.restore();
+
+    //jambe droite
+    objC2D.save();
+    objC2D.rotate(-7 * Math.PI / 16);
+    objC2D.fillRect(-22, 10, 3.5, 7);
+    objC2D.rotate(-7 * Math.PI / 16);
+    objC2D.fillRect(-22, -23, 3.5, 8);
+    objC2D.restore();
+
+    objC2D.restore();
+
     objC2D.restore();
 }

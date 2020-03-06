bbcd10ebce3d0a0a0c0941b386e1174b7fe59473
Caroline <carolineboudreau@live.ca>
2020-03-05 21:34:30 -0500

    animation Lode Runner

--- Lode_Runner/mouvementLR.js
+++ Lode_Runner/mouvementLR.js
@@ -107,6 +107,9 @@ function hautBas(){
     if(binNiveauReussi){
         niveauReussi();
     }
+
+
+    objLodeRunner.intNbDeplacementV=objLodeRunner.intNbDeplacementV+1;
 }
 
 function gaucheDroiteEchelle(){
--- Lode_Runner/personnage.js
+++ Lode_Runner/personnage.js
@@ -4,31 +4,35 @@ function dessinerGardes() {
     }
 }
 
-
-
 function dessinerLodeRunner() {
-    if (objLodeRunner.intNbDeplacementH % 2 != 0) {
+    objC2D.save();
 
-        objC2D.save();
+    objC2D.fillStyle = '#a09794';
+    objC2D.translate(objLodeRunner.posX, objLodeRunner.posY);
 
-        objC2D.fillStyle = '#a09794';
-        objC2D.translate(objLodeRunner.posX, objLodeRunner.posY);
+    //Changer de côté ?
+    if (objLodeRunner.intDirection == -1) {
+        objC2D.scale(-1, 1);
+        objC2D.translate(-30, 0);
+    }
 
-        //Changer de côté ?
-        if (objLodeRunner.intDirection == -1) {
-            objC2D.scale(-1, 1);
-            objC2D.translate(-30, 0);
-        }
+    objC2D.beginPath();
+    objC2D.arc(15, 6, 5, 0, 2 * Math.PI);
+    objC2D.fill();
 
-        objC2D.beginPath();
-        objC2D.arc(15, 6, 5, 0, 2 * Math.PI);
-        objC2D.fill();
+    objC2D.fillStyle = objLodeRunner.strCouleur;
 
-        objC2D.fillStyle = objLodeRunner.strCouleur;
+    //milieu du corps
+    objC2D.fillRect(13, 11, 4.5, 10);
 
-        //milieu du corps
-        objC2D.fillRect(13, 11, 4.5, 10);
+    dessinerLodeRunnerHorizontal();
+    dessinerLodeRunnerVertical();
+   
+}
 
+function dessinerLodeRunnerHorizontal(){
+    
+    if (objLodeRunner.intNbDeplacementH % 2 != 0 &&  objLodeRunner.etat == 0) {
         //bras gauche
         objC2D.save();
         objC2D.rotate(2 * Math.PI / 16);
@@ -69,35 +73,83 @@ function dessinerLodeRunner() {
 
         objC2D.restore();
 
-    } else if (objLodeRunner.intNbDeplacementH % 2 == 0) {
+    } else if (objLodeRunner.intNbDeplacementH % 2 == 0 &&  objLodeRunner.etat == 0) {
+
+        //bras gauche
         objC2D.save();
+        objC2D.rotate(6 * Math.PI / 16);
+        objC2D.fillRect(17, -4, 5.5, 3.3);
+        objC2D.restore();
+        objC2D.save();
+        objC2D.rotate(-1.5 * Math.PI / 8);
+        objC2D.fillRect(-1, 17, 5, 3.3);
+
+        objC2D.restore();
+
+        //bras droit
+        objC2D.save();
+        objC2D.rotate(3.5 * Math.PI / 16);
+        objC2D.fillRect(21, -1, 5.5, 3.3);
+        objC2D.restore();
+        objC2D.save();
+        objC2D.rotate(-2 * Math.PI / 8);
+        objC2D.fillRect(1, 24, 5, 3.3);
+        objC2D.restore();
 
         objC2D.fillStyle = '#a09794';
-        objC2D.translate(objLodeRunner.posX, objLodeRunner.posY);
 
-        //Changer de côté ?
-        if (objLodeRunner.intDirection == -1) {
+        //jambe droite
+        objC2D.save();
+        objC2D.rotate(-2 * Math.PI / 16);
+        objC2D.fillRect(5, 26, 3.5, 5);
+        objC2D.rotate(3 * Math.PI / 16);
+        objC2D.fillRect(21, 21, 3.5, 5);
+        objC2D.restore();
+
+        //jambe gauche
+        objC2D.save();
+        objC2D.rotate(2 * Math.PI / 16);
+        objC2D.fillRect(20, 13, 3.5, 7);
+        objC2D.rotate(2 * Math.PI / 16);
+        objC2D.fillRect(26, 10, 3.5, 5);
+        objC2D.restore();
+
+        objC2D.restore();
+    }else if (objLodeRunner.etat==3){
+       //TODO : modifier dessin jambes quand il est sur barre de franchissement
+
+        objC2D.fillStyle = '#a09794';
+
+        //jambe droite
+        objC2D.save();
+        objC2D.rotate(-2 * Math.PI / 16);
+        objC2D.fillRect(5, 26, 3.5, 5);
+        objC2D.rotate(3 * Math.PI / 16);
+        objC2D.fillRect(21, 22, 3.5, 5);
+        objC2D.restore();
+
+        //jambe gauche
+        objC2D.save();
+        objC2D.rotate(2 * Math.PI / 16);
+        objC2D.fillRect(20, 13, 3.5, 7);
+        objC2D.rotate(2 * Math.PI / 16);
+        objC2D.fillRect(26, 10, 3.5, 5);
+        objC2D.restore();
+        
+        if (objLodeRunner.intNbDeplacementH % 2 ==0){
             objC2D.scale(-1, 1);
             objC2D.translate(-30, 0);
         }
 
-        objC2D.beginPath();
-        objC2D.arc(15, 6, 5, 0, 2 * Math.PI);
-        objC2D.fill();
-
         objC2D.fillStyle = objLodeRunner.strCouleur;
-
-        //milieu du corps
-        objC2D.fillRect(13, 11, 4.5, 10);
-
         //bras gauche
         objC2D.save();
-        objC2D.rotate(2 * Math.PI / 16);
-        objC2D.fillRect(3, 6, 5.5, 3.3);
+        objC2D.rotate(6 * Math.PI / 16);
+        objC2D.fillRect(17, -4, 5.5, 3.3);
         objC2D.restore();
         objC2D.save();
-        objC2D.rotate(-1 * Math.PI / 8);
-        objC2D.fillRect(-1, 13, 5, 3.3);
+        objC2D.rotate(-1.5 * Math.PI / 8);
+        objC2D.fillRect(-1, 17, 5, 3.3);
 
         objC2D.restore();
 
@@ -111,24 +163,104 @@ function dessinerLodeRunner() {
         objC2D.fillRect(5, 22, 5, 3.3);
         objC2D.restore();
 
-        objC2D.fillStyle = '#a09794';
-        //jambe gauche
-        objC2D.save();
-        objC2D.rotate(3.5 * Math.PI / 16);
-        objC2D.fillRect(22, 7, 3.5, 7);
-        objC2D.rotate(7 * Math.PI / 16);
-        objC2D.fillRect(16, -22, 3.5, 7);
         objC2D.restore();
+    }
+}
 
-        //jambe droite
-        objC2D.save();
-        objC2D.rotate(-7 * Math.PI / 16);
-        objC2D.fillRect(-20, 18, 3.5, 7);
-        objC2D.rotate(-7 * Math.PI / 16);
-        objC2D.fillRect(-30, -20, 3.5, 8);
-        objC2D.restore();
+function dessinerLodeRunnerVertical(){
+    if (objLodeRunner.etat == 1){
+        
+    if (objLodeRunner.intNbDeplacementV % 2 == 0 ) {
+        objC2D.scale(-1, 1);
+        objC2D.translate(-30, 0);
+    }
+     //bras gauche
+     objC2D.save();
+     objC2D.rotate(7 * Math.PI / 16);
+     objC2D.fillRect(14, -7, 5.5, 3.3);
+     objC2D.restore();
+     objC2D.save();
+     objC2D.rotate(-2 * Math.PI / 8);
+     objC2D.fillRect(-5, 17, 5, 3.3);
 
-        objC2D.restore();
+     objC2D.restore();
+
+     //bras gauche
+     objC2D.save();
+     objC2D.rotate(-4 * Math.PI / 16);
+     objC2D.fillRect(2,21, 5,3.3);
+     objC2D.restore();
+     objC2D.save();
+     objC2D.rotate(-2.5 * Math.PI / 8);
+     objC2D.fillRect(2, 22, 5, 3.3);
+     objC2D.restore();
+
+     objC2D.fillStyle = '#a09794';
+
+     //jambe gauche
+     objC2D.save();
+     objC2D.rotate(1.5 * Math.PI / 16);
+     objC2D.fillRect(25, 10, 3.5, 7);
+     objC2D.rotate(2 * Math.PI / 16);
+     objC2D.fillRect(26, 0, 3.5, 8);
+     objC2D.restore();
+
+     //jambe droite
+     objC2D.save();
+     objC2D.rotate(1 * Math.PI / 16);
+     objC2D.fillRect(17, 22, 3.5, 7);
+     objC2D.rotate(2 * Math.PI / 16);
+     objC2D.fillRect(24, 9, 4, 5);
+     objC2D.restore();
+
+     objC2D.restore();
+    } else if (objLodeRunner.etat == 2){
+
+    //TODO : modifier dessin quand il tombe     
+        if (objLodeRunner.intNbDeplacementV % 2 == 0 ) {
+            objC2D.scale(-1, 1);
+            objC2D.translate(-30, 0);
+        }
+         //bras gauche
+         objC2D.save();
+         objC2D.rotate(7 * Math.PI / 16);
+         objC2D.fillRect(14, -7, 5.5, 3.3);
+         objC2D.restore();
+         objC2D.save();
+         objC2D.rotate(-2 * Math.PI / 8);
+         objC2D.fillRect(-5, 17, 5, 3.3);
+    
+         objC2D.restore();
+    
+         //bras gauche
+         objC2D.save();
+         objC2D.rotate(-4 * Math.PI / 16);
+         objC2D.fillRect(2,21, 5,3.3);
+         objC2D.restore();
+         objC2D.save();
+         objC2D.rotate(-2.5 * Math.PI / 8);
+         objC2D.fillRect(2, 22, 5, 3.3);
+         objC2D.restore();
+    
+         objC2D.fillStyle = '#a09794';
+    
+         //jambe gauche
+         objC2D.save();
+         objC2D.rotate(1.5 * Math.PI / 16);
+         objC2D.fillRect(25, 10, 3.5, 7);
+         objC2D.rotate(2 * Math.PI / 16);
+         objC2D.fillRect(26, 0, 3.5, 8);
+         objC2D.restore();
+    
+         //jambe droite
+         objC2D.save();
+         objC2D.rotate(1 * Math.PI / 16);
+         objC2D.fillRect(17, 22, 3.5, 7);
+         objC2D.rotate(2 * Math.PI / 16);
+         objC2D.fillRect(24, 9, 4, 5);
+         objC2D.restore();
+    
+         objC2D.restore();
     }
 }
 
